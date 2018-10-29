e60c9f510e705a5ea1fdefa2ba09bcb1b40e4c29
ariua91 <arianto.kor@gmail.com>
2018-10-29 23:01:31 +0800

    halfway through db flask tutorial

--- config.py
+++ config.py
@@ -5,8 +5,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = ';lkdsajfeoiwur438975fjds'
-
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class ProductionConfig(Config):
--- trello_parser/app.py
+++ trello_parser/app.py
@@ -1,10 +1,14 @@
-import os
 from flask import Flask, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+import os
 from webhook_parser import *
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-print(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Result
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
