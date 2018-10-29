a87f4b2bee952ba0a851d0cd73c701ba46af8735
ariua91 <arianto.kor@gmail.com>
2018-10-29 21:13:09 +0800

    downgrading itsdagerous for heroku

--- trello_parser/app.py
+++ trello_parser/app.py
@@ -17,6 +17,11 @@ def webhook():
 def hello():
     return "Hello World!"
 
+@app.route('/<name>')
+def hello_name(name):
+    return 'Hello %s'%name
+
+
 if __name__ == '__main__':
     app.run()
 
